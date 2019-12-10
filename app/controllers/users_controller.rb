fa43264863b028791af6586f753f204adb1083d7
yusuke.fujinaga <i7love1myhome9@gmai.com>
2019-12-10 05:18:45 +0000

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+  	@new = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to root_path
+      
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
    root to: 'tasks#index'  
   
    resources :tasks
+   
+   get 'signup', to: 'users#new'
+   resources :users, only: [:new, :create]
   
 end
