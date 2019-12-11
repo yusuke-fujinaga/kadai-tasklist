19a04b5b45be28ff3f1aabc7991eb3d7ccb126a5
yusuke.fujinaga <i7love1myhome9@gmai.com>
2019-12-11 03:19:43 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,9 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
   
   def show
@@ -14,7 +16,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:saccess] = 'Taskが正常に投稿されました'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
+	has_many :tasks
+	
 	before_save { self.email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 },
